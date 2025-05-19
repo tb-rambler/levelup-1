f35fd9ced030ac1f2a0853fcfe24f9850c286773
tb-rambler <mount0@rambler.ru>
2025-05-20 00:19:42 +0300

    dz1

--- les1/practice.py
+++ les1/practice.py
@@ -4,5 +4,24 @@ y = int(input())
 sum = x + y
 print(sum)
 
-print("напишите строчку: ")
-stroka = (input())
+line = int(input("напишите строчку: "))
+print(type(line))
+
+a = int(input())
+b = int(input())
+print(a - b)
+
+a = float(input())
+b = float(input())
+res1 = a * b
+res2 = a / b
+print(res1, res2)
+
+line1 = input()
+line2 = input()
+print(line1 + ' ' + line2)
+
+string = int(input())
+digit = 505
+result = string + digit
+print(result)
