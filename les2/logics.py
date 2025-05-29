c3a117dfd6caeeaa637cdfb62dff0cb36cca4519
tb-rambler <mount0@rambler.ru>
2025-05-30 01:34:29 +0300

    испр

--- les2/logics.py
+++ les2/logics.py
@@ -8,6 +8,8 @@
 # else:
 #      print("сегодня также")
 
+# and, or, not
+# ################
 
 # let1 = input("1st letter ABC ")
 # let2 = input("last letter ABC ")
@@ -24,6 +26,8 @@ x = 5
 # if 0 <= x < 100: # верный красивый код
 #    pass
 
+# UTF-8
+
 # let1 = "t"
 # let2 = "w"
 # print(let1 > let2)
@@ -31,12 +35,15 @@ x = 5
 # print(ord(let2))
 # print(chr(116)) # код заглавной буквы
 # print(chr(119))
+# print(ord('W'))
 
 # text = input()
 # if "добр" in text:
 #    print("встретилось 'доброе' слово")
 # else:
 #    print("добрых слов для вас нет")   
+
+#XOR - ^
 
 a = True
 b = False
@@ -49,3 +56,14 @@ print(a, 'XOR', a, '=', p)
 print(a, 'XOR', b, '=', q)
 print(b, 'XOR', a, '=', r)
 print(b, 'XOR', b, '=', s)
+
+# print("{0:b}".format(15))
+
+print(15^32)
+print(bin(47))
+print(bin(15))
+print(bin(32))
+
+15 = 001111
+32 = 100000
+XOR = 101111
