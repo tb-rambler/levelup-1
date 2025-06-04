c25f8e3177310a12567f95b49423310a31873c23
tb-rambler <mount0@rambler.ru>
2025-06-05 01:17:14 +0300

    7 задача из 3 ДЗ готова

--- les3/practice3.py
+++ les3/practice3.py
@@ -1,59 +1,59 @@
 # 1. ... вычисляет сумму всех чисел от 1 до 100 включительно используя for. 
 # результат вывести на экран
 
-# total = 0
-# for i in range(1, 101):
-#       total += i
-# print(total)
+total = 0
+for i in range(1, 101):
+      total += i
+print(total)
 
 # # # 2. запрашивает у пользователя строку и выводит каждый символ в новой строке (for)
 
-# line = input('Введите стоку: ')
-# for i in line:
-#    print(i)
+line = input('Введите стоку: ')
+for i in line:
+   print(i)
 
 # # 3. запрашивает число у юзера и выводит таблицу умножения этого числа от 1 до 15
 
-# dig = int(input('задайте число: '))
-# for i in range(1, 16):
-#    i = dig * i
-#    print(i)
+dig = int(input('задайте число: '))
+for i in range(1, 16):
+   i = dig * i
+   print(i)
 
 # # 4. запрашивает у пользователя число n и вычисляет сумму всех четных чисел от 1 до n включительно. Используйте цикл for и условие.
    
-# n = int(input('задайте число: '))
-# total = 0
-# for i in range(1, n + 1):
-#    if i % 2 == 0:
-#       total += i
-# print(total)
+n = int(input('задайте число: '))
+total = 0
+for i in range(1, n + 1):
+   if i % 2 == 0:
+      total += i
+print(total)
 
 # # 5. запрашивает у пользователя число n и находит сумму всех чисел от 1 до n, которые делятся на 3 без остатка. Используйте цикл while.
 
-# n = int(input('задайте число: '))
-# total = 0
-# i = 1
-# while i <= n:
-#    if i % 3 == 0:
-#       total += i
-#    i += 1
-# print(total)
+n = int(input('задайте число: '))
+total = 0
+i = 1
+while i <= n:
+   if i % 3 == 0:
+      total += i
+   i += 1
+print(total)
 
 # # 6. Напишите программу, которая запрашивает у пользователя число и проверяет,
 # #  является ли оно простым. Число называется простым, если оно делится только 
 # # на 1 и на само себя. Используйте цикл for для проверки делимости.
 
-# n = int(input('задайте число: \n'))
-# if n <= 1:
-#    print("числа могут быть только > 1")
-# else:
-#    for i in range(n-1, 1, -1):
-#       if n % i != 0:
-#          continue 
-#       print('Число составное')
-#       break
-#    else:   
-#       print('Число простое')   
+n = int(input('задайте число: \n'))
+if n <= 1:
+   print("числа могут быть только > 1")
+else:
+   for i in range(n-1, 1, -1):
+      if n % i != 0:
+         continue 
+      print('Число составное')
+      break
+   else:   
+      print('Число простое')   
        
 
 # 7. Напишите программу, которая запрашивает у пользователя строку и проверяет, 
@@ -62,19 +62,16 @@
 phrase = input('Введите фразу: \n')
 
 phrase_n = phrase.lower() # перевод фразы в нижний рег.
-# print(phrase_n)
+phrase_del = phrase_n.replace(' ', '') # убрать пробелы
+phrase_rev = phrase_del[::-1] # переворот стоки
 
-long = len(phrase) # длина фразы
-# print(long)
+list_1 = [phrase_del]
+list_2 = [phrase_rev]
 
-phrase_rev = phrase_n[::-1] # переворот стоки
-print(phrase_rev)
+# print(list_1)
+# print(list_2)
 
-# if i in phrase_n = j in phrase_rev:
-#    print('это палиандром')
-# print('это фигня')
-
-
-
-
-
+if list_1 == list_2:
+   print('это палиандром')
+else:
+   print('это фигня')
